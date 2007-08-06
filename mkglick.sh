9d50068b183f7aae254b490530ff5d4864edc7d5
Alexander Larsson <alexl@redhat.com>
2007-08-06 23:21:22 +0200

    Statically link ext2fs

--- mkglick.sh
+++ mkglick.sh
@@ -3,5 +3,9 @@ cp $1 image
 ld -r -b binary -o image.o image
 rm image
 objcopy --rename-section .data=.rodata,alloc,load,readonly,data,cont image.o
-gcc header.a image.o `pkg-config --libs fuse ext2fs` -o $2
+# Dynamic ext2fs
+EXT2FS_LIB=`pkg-config --libs ext2fs`
+# Static ext2fs
+EXT2FS_LIB="/usr/lib/libext2fs.a /usr/lib/libcom_err.a" 
+gcc header.a image.o $EXT2FS_LIB `pkg-config --libs fuse` -o $2
 
