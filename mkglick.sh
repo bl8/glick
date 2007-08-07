026c6d65da518eead11fda25c3a7571d1c8c8800
Alexander Larsson <alexl@redhat.com>
2007-08-07 16:08:59 +0200

    strip glick files

--- mkglick.sh
+++ mkglick.sh
@@ -8,4 +8,5 @@ EXT2FS_LIB=`pkg-config --libs ext2fs`
 # Static ext2fs
 EXT2FS_LIB="/usr/lib/libext2fs.a /usr/lib/libcom_err.a" 
 gcc header.a image.o $EXT2FS_LIB `pkg-config --libs fuse` -o $2
+strip $2
 
