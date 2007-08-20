7e56aed9d3501dcfb6a6badcaa634afc480187b7
Alexander Larsson <alexl@redhat.com>
2007-08-20 14:23:49 +0200

    Make mkglick not write in the cwd

--- mkglick.sh
+++ mkglick.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cp $1 image
-ld -r -b binary -o image.o image
-rm image
-objcopy --rename-section .data=.rodata,alloc,load,readonly,data,cont image.o
+DIR=`mktemp -d`
+cp $1 $DIR/image
+(cd $DIR; ld -r -b binary -o image.o image)
+objcopy --rename-section .data=.rodata,alloc,load,readonly,data,cont $DIR/image.o
 # Dynamic ext2fs
 EXT2FS_LIB=`pkg-config --libs ext2fs`
 # Static ext2fs
 EXT2FS_LIB="/usr/lib/libext2fs.a /usr/lib/libcom_err.a" 
-gcc header.a image.o $EXT2FS_LIB `pkg-config --libs fuse` -o $2
+gcc header.a $DIR/image.o $EXT2FS_LIB `pkg-config --libs fuse` -o $2
+rm -rf $DIR
 strip $2
-
